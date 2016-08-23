07c02bbc291cf96bcfc440a9a5c69e9292fcc5ad
Kritro21 <3332425262@qq.com>
2016-08-24 07:28:55 +0800

    Update Kritro.sh

--- Kritro.sh
+++ Kritro.sh
@@ -1,37 +1,39 @@
 #!/bin/sh
-#  Kritro.sh
+
+#  installDOFCentOS.sh
+#  
 #
-#  Created by Kritro on 16/8/23.
+#  Created by command.Zi on 16/5/11.
 #
 
 function install() {
-    echo "*****************************************************************"
+    eecho "*****************************************************************"
 	echo " Welcome you use Kritro write a key erection DNF service script"                  
 	echo "          感谢您使用Kritro一键架设DNF商业版!"
 	echo "           Our web site：www.1nvincible.online"
 	echo "              我们的网站：1nvincible.online"
 	echo "         Created by Kritro on 23/8/11.最后一次整理!"
 	echo "******************************************************************"
-    read -p "请输入：CentOS版本，例如5.11	输入5 然后回车：" versionNumber
+    read -p "请输入CentOS版本 例如5.11，输入5回车：" versionNumber
 #TODO:直接取系统版本号判断，再检测文件是否存在，不符合都跳出
-    read -p "输入服务器环境，2.国外(需要自行更换证书及pvf文件) 3.自带Kritro.tar.gz及“公钥”及“PVF”文件(此项开始前要确保根目录下存在Server.tar.gz、publickey.pem、Script.pvf)优先选3 回车：" networkState
+    read -p "输入服务器环境，2为国外(需要自行更换证书及pvf文件)，3为自带Server.tar.gz及证书及pvf文件(此项开始前要确保根目录下存在Server.tar.gz、publickey.pem、Script.pvf)，优先选3，然后回车：" networkState
     if (($versionNumber==5)); then
         installSupportLibOnCentOS5
     elif (($versionNumber==6)); then
         installSupportLibOnCentOS6
     else
-        echo "仅支持CentOS 5.x及6.x"
+        echo "其实只有5和6"
         exit 0
     fi
     echo "添加交换空间，耐心等待?……"
     addSwap
-    Kritro
+    installDOF
     deleteRoot6686
     removeTemp
 }
 
 function getIP() {
-    echo "正在获取当前服务器IP..."
+    echo "获取 IP..."
     IP=`curl -s http://v4.ipv6-test.com/api/myip.php`
     if [ -z $IP ]; then
     IP=`curl -s https://www.boip.net/api/myip`
@@ -39,18 +41,18 @@ function getIP() {
 }
 
 function addSwap() {
-    echo "正在设置Swap..."
-#   if read -n1 -p "请输入虚拟内存大小(正整数、单位为GB、默认8GB)" answer
+    echo "添加 Swap..."
+#   if read -n1 -p "请输入虚拟内存大小（正整数、单位为GB、默认6  GB）" answer
 #   then
-#   /bin/dd if=/dev/zero of=/var/swap.Kritro bs=1M count=1000*$answer
-    /bin/dd if=/dev/zero of=/var/swap.Kritro bs=1M count=8000
-    mkswap /var/swap.Kritro
-    swapon /var/swap.Kritro
+#   /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1000*$answer
+    /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=8000
+    mkswap /var/swap.1
+    swapon /var/swap.1
 #   加入开机自动挂载
 #   $ 最后一行
 #   a 在该指令前面的行数后面插入该指令后面的内容
-    sed -i '$a /var/swap.Kritro swap swap default 0 0' /etc/fstab
-    echo "添加Swap成功!请继续!"
+    sed -i '$a /var/swap.1 swap swap default 0 0' /etc/fstab
+    echo "添加 Swap 成功"
 }
 
 function installSupportLibOnCentOS5() {
@@ -93,33 +95,33 @@ function installSupportLibOnCentOS6() {
 
 function installDOF() {
     getIP
-    echo -n "${IP} 是否是你的外网IP？(如果不是你的外网IP或者出现两条IP地址,请自行输入n回车输入IP!!) y/n [n] ?"
+    echo -n "${IP} 是否是你的外网IP？(如果不是你的外网IP或者出现两条IP地址，请回 n 自行输入) y/n [n] ?"
     read ans
     case $ans in
     y|Y|yes|Yes)
     ;;
     n|N|no|No)
-    read -p "输入你的外网IP地址,回车!(确保是英文字符的点号)：" myip
+    read -p "输入你的外网IP地址，回车（确保是英文字符的点号）：" myip
     IP=$myip
     ;;
     *)
     ;;
     esac
     cd ~
-    echo "下载Server FOR DNF..."
+    echo "下载Server..."
     if (($networkState==5)); then
         cd ~
-    #   Kritro
+    #   七牛
     elif (($networkState==3)); then
         cd ~
     else
-        wget -O /root/Kritro.tar.gz https://www.dropbox.com/s/9fz5grju3xf2q8c/Server.tar.gz?dl=0
+        wget -O /root/Server.tar.gz https://www.dropbox.com/s/9fz5grju3xf2q8c/Server.tar.gz?dl=0
         wget -O /root/Script.pvf https://www.dropbox.com/s/ofu0d6owm6h3igy/Script.pvf?dl=0
         wget -O /root/publickey.pem https://www.dropbox.com/s/u2q0s5t56wvkk7l/publickey.pem?dl=0
     fi
-    cp Kritro.tar.gz /
+    cp Server.tar.gz /
     cd /
-    tar -zvxf Kritro.tar.gz
+    tar -zvxf Server.tar.gz
     tar -zvxf var.tar.gz
     cd /home/GeoIP-1.4.8/
     ./configure
@@ -155,7 +157,7 @@ function deleteRoot6686() {
     HOSTNAME="127.0.0.1"
     PORT="3306"
     USERNAME="game"
-    PASSWORD="Kritro21"
+    PASSWORD="uu5!^%jg"
     DBNAME="mysql"
     TABLENAME="user"
     refresh="flush privileges;";
@@ -167,14 +169,14 @@ function deleteRoot6686() {
 }
 
 function removeTemp() {
-    echo -n -t 5 "架设完成!!是否删除临时文件 y/n [n] ?"
+    echo -n -t 5 "完成安装，是否删除临时文件 y/n [n] ?"
     read ANS
     case $ANS in
     y|Y|yes|Yes)
     rm -f /root/mysql57*
     rm -f /var.tar.gz
     rm -f /etc.tar.gz
-    rm -f /Kritro.tar.gz
+    rm -f /Server.tar.gz
     ;;
     n|N|no|No)
     ;;
@@ -185,7 +187,7 @@ function removeTemp() {
 
 install
 echo "********************************************************"
-echo "                   IP = ${IP}"
+echo " IP = ${IP}"
 echo "          感谢您使用Kritro一键架设DNF商业版!"
 echo "           我们的网站：www.1nvincible.online"
 echo "              制作者：Kritro QQ 3332425262"
