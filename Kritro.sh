a90b49f9c13c75429163241d54361251eae72587
Kritro21 <3332425262@qq.com>
2016-08-24 08:04:51 +0800

    Update Kritro.sh

--- Kritro.sh
+++ Kritro.sh
@@ -1,33 +1,39 @@
 #!/bin/sh
 
-#  installDOFCentOS.sh
+#  Kritro.sh
 #  
 #
-#  Created by command.Zi on 16/5/11.
+#  Created by Kritro on 16/5/11.
 #
 
 function install() {
-    echo "腾讯云的朋友，可以一试，确定按照这里的操作无误，还是无法成功，另找他法吧"
-    read -p "输入centOS版本，例如5.11，输入5，然后回车：" versionNumber
+    echo "*****************************************************************"
+	echo " Welcome you use Kritro write a key erection DNF service script"                  
+	echo "          感谢您使用Kritro一键架设DNF商业版!"
+	echo "           Our web site：www.1nvincible.online"
+	echo "              我们的网站：1nvincible.online"
+	echo "         Created by Kritro on 23/8/11.最后一次整理!"
+	echo "******************************************************************"
+    read -p "请输入Linux CentOS版本	例如5.11 输入“5”回车：" versionNumber
 #TODO:直接取系统版本号判断，再检测文件是否存在，不符合都跳出
-    read -p "输入服务器环境，2为国外(需要自行更换证书及pvf文件)，3为自带Server.tar.gz及证书及pvf文件(此项开始前要确保根目录下存在Server.tar.gz、publickey.pem、Script.pvf)，优先选3，然后回车：" networkState
+    read -p "请选择服务器环境，2.国外(需要自行更换秘钥及PVF文件) 3.自备Server.tar.gz及公钥及PVF文件(此项开始前要确保根目录下存在Server.tar.gz、publickey.pem、Script.pvf)，优先选”3“回车：" networkState
     if (($versionNumber==5)); then
         installSupportLibOnCentOS5
     elif (($versionNumber==6)); then
         installSupportLibOnCentOS6
     else
-        echo "其实只有5和6"
+        echo "仅支持只有5.x和6.x"
         exit 0
     fi
-    echo "添加交换空间，耐心等待?……"
+    echo "添加虚拟内存 耐心等待……"
     addSwap
-    installDOF
+    Kritro
     deleteRoot6686
     removeTemp
 }
 
 function getIP() {
-    echo "获取 IP..."
+    echo "正在获取当前服务器IP..."
     IP=`curl -s http://v4.ipv6-test.com/api/myip.php`
     if [ -z $IP ]; then
     IP=`curl -s https://www.boip.net/api/myip`
@@ -35,22 +41,22 @@ function getIP() {
 }
 
 function addSwap() {
-    echo "添加 Swap..."
+    echo "添加虚拟内存(Swap)..."
 #   if read -n1 -p "请输入虚拟内存大小（正整数、单位为GB、默认6  GB）" answer
 #   then
-#   /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1000*$answer
-    /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=8000
-    mkswap /var/swap.1
-    swapon /var/swap.1
-#   加入开机自动挂载
+#   /bin/dd if=/dev/zero of=/var/swap.Kritro bs=1M count=1000*$answer
+    /bin/dd if=/dev/zero of=/var/swap.Kritro bs=1M count=8000
+    mkswap /var/swap.Kritro
+    swapon /var/swap.Kritro
+#   加入开机自启动
 #   $ 最后一行
 #   a 在该指令前面的行数后面插入该指令后面的内容
-    sed -i '$a /var/swap.1 swap swap default 0 0' /etc/fstab
-    echo "添加 Swap 成功"
+    sed -i '$a /var/swap.Kritro swap swap default 0 0' /etc/fstab
+    echo "添加虚拟内存(Swap) Success!!"
 }
 
 function installSupportLibOnCentOS5() {
-    echo "安装运行库..."
+    echo "正在安装运行库for CentOS 5.x..."
     if (($networkState==3)); then
         wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-5.repo
         yum clean all
@@ -70,7 +76,7 @@ function installSupportLibOnCentOS5() {
 }
 
 function installSupportLibOnCentOS6() {
-    echo "安装运行库..."
+    echo "正在安装运行库for CentOS 6.x..."
     if (($networkState==1)); then
         wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-6.repo
         yum clean all
@@ -89,13 +95,13 @@ function installSupportLibOnCentOS6() {
 
 function installDOF() {
     getIP
-    echo -n "${IP} 是否是你的外网IP？(如果不是你的外网IP或者出现两条IP地址，请回 n 自行输入) y/n [n] ?"
+    echo -n "${IP} 是否是你的外网IP?(如果不是你的外网IP或者出现两条IP地址 请输入 n 回车后自行输入!) y/n [n] ?"
     read ans
     case $ans in
     y|Y|yes|Yes)
     ;;
     n|N|no|No)
-    read -p "输入你的外网IP地址，回车（确保是英文字符的点号）：" myip
+    read -p "输入你的外网IP地址 回车（确保是英文字符的点号）：" myip
     IP=$myip
     ;;
     *)
@@ -105,7 +111,7 @@ function installDOF() {
     echo "下载Server..."
     if (($networkState==5)); then
         cd ~
-    #   七牛
+    #   Kritro
     elif (($networkState==3)); then
         cd ~
     else
@@ -163,7 +169,7 @@ function deleteRoot6686() {
 }
 
 function removeTemp() {
-    echo -n -t 5 "完成安装，是否删除临时文件 y/n [n] ?"
+    echo -n -t 5 "架设完成!!是否删除临时文件 y/n [n] ?"
     read ANS
     case $ANS in
     y|Y|yes|Yes)
@@ -180,7 +186,10 @@ function removeTemp() {
 }
 
 install
-echo "***********************"
+echo "********************************************************"
 echo " IP = ${IP}"
-echo "重启的话需要使用命令 service iptables stop 重新关闭防火墙"
-echo "***********************"
+echo "          感谢您使用Kritro一键架设DNF商业版!"
+echo "           我们的网站：www.1nvincible.online"
+echo "              制作者：Kritro QQ 3332425262"
+echo "         Created by Kritro on 23/8/11.最后一次整理!"
+echo "********************************************************"
